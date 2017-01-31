68551c1342ff3bc82b312ca700c0dccfcb03cdf5
hafizhindram <hafizhindram@gmail.com>
2017-01-31 09:09:13 +0700

    Tugas Pertama Saya

--- JavaClass/app/src/main/java/hafizh/him/javaclass/MainActivity.java
+++ JavaClass/app/src/main/java/hafizh/him/javaclass/MainActivity.java
@@ -2,12 +2,23 @@ package hafizh.him.javaclass;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    public String showString;
+    public TextView showText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        showText = (TextView) findViewById(R.id.show_text);
+
+        showString = "Manipulasi class java android";
+        addSeparator();
+        showText.setText(showString);
+    }
+    public void addSeparator(){
+        showString += "\n-----------------------------------------------\n";
     }
 }
