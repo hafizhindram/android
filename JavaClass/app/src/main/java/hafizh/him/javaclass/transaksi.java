a23701f8d0504223f42616b6099e3ff52ff31a1b
hafizhindram <hafizhindramajid@gmail.com>
2017-02-01 08:13:35 +0700

    Java Class

--- JavaClass/app/src/main/java/hafizh/him/javaclass/transaksi.java
+++ JavaClass/app/src/main/java/hafizh/him/javaclass/transaksi.java
@@ -1,11 +1,45 @@
 package hafizh.him.javaclass;
 
+import java.util.ArrayList;
+
 /**
  * Created by D2J-00 on 31/01/2017.
  */
 public class transaksi {
 
+    private ArrayList<barang> ListBarang = new ArrayList<>();
 
+    public  void addBarang(barang barang)
+    {
+        this.ListBarang.add(barang);
+    }
+
+    public int totalTransaksi()
+    {
+        int hasil=0;
+        for (int i=0; i < ListBarang.size();i++)
+        {
+            hasil += ListBarang.get(i).getHarga();
+        }
+
+        return hasil;
+    }
+
+    public String printTransaksi()
+    {
+        String text = "Barang yang dibeli pada transaksi ini adalah: \n";
+        text +="\n-------------------------------------------------------\n";
+
+        for (int i=0; i<ListBarang.size();i++)
+        {
+            text += ListBarang.get(i).toString();
+        }
+        text +="\n-------------------------------------------------------\n";
+        text +="Total pembelian : "+totalTransaksi()+"\n";
+        text +="\n-------------------------------------------------------\n";
+
+        return text;
+    }
 
 
 }
