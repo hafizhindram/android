72527dff269fcc7f27d91844f84aec704334b766
hafizhindram <hafizhindramajid@gmail.com>
2017-02-03 10:05:37 +0700

    Multifunction Application

---
+++ MultiFunctionApp/app/src/main/java/hafizh/him/multifunctionapp/MainActivity.java
@@ -0,0 +1,46 @@
+package hafizh.him.multifunctionapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    public final static String EXTRA = "hafizh.him.multifunctionapp.Main";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+    public void ShowMessage(View view){
+      //contoh penggunaan explicit intent
+        Intent intent = new Intent(this, ShowMessage.class);
+        TextView txtMessage = (TextView) findViewById(R.id.txt_message);
+        intent.putExtra(EXTRA,txtMessage.getText().toString());
+        startActivity(intent);
+    }
+    public void openBrowser(View view){
+        //membuka activity yang akan mengeksekusi implicit intent
+        Intent intent = new Intent(this, IntentExecutor.class);
+        intent.putExtra(EXTRA, "Browser");
+        startActivity(intent);
+    }
+    public void openPhone(View view){
+        //membuka activity yang akan mengeksekusi implicit intent
+        Intent intent = new Intent(this, IntentExecutor.class);
+        intent.putExtra(EXTRA, "phone");
+        startActivity(intent);
+
+    }
+    public void openMap(View view){
+        //membuka activity yang akan mengeksekusi implicit intent
+        Intent intent = new Intent(this, IntentExecutor.class);
+        intent.putExtra(EXTRA, "map");
+        startActivity(intent);
+
+    }
+}
