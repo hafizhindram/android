60bf1a0210a4960a420e46ba430b8cb9da0f61d1
hafizhindram <hafizhindramajid@gmail.com>
2017-02-13 10:41:36 +0700

    Aplikasi Keuangan

---
+++ AplikasiKeuangan/app/src/main/java/hafizh/him/aplikasikeuangan/FormActivity.java
@@ -0,0 +1,50 @@
+package hafizh.him.aplikasikeuangan;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+public class FormActivity extends AppCompatActivity {
+
+    private String [] jenisStr = {"Pemasukan", "Pengeluaran"};
+    private EditText edtNama, edtJumlah, edtKeterangan;
+    private Spinner spnJenis;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form);
+
+        spnJenis = (Spinner) findViewById(R.id.jenis);
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, jenisStr);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spnJenis.setAdapter(adapter);
+
+        edtNama = (EditText) findViewById(R.id.nama);
+        edtJumlah = (EditText) findViewById(R.id.jumlah);
+        edtKeterangan = (EditText) findViewById(R.id.keterangan);
+    }
+
+    public void tambah(View view){
+        TransaksiHelper dbHelper = new TransaksiHelper(this);
+        String nama = edtNama.getText().toString();
+        int jenis = spnJenis.getSelectedItemPosition()+1;
+        int jumlah = Integer.parseInt(edtJumlah.getText().toString());
+        String keterangan = edtKeterangan.getText().toString();
+
+        dbHelper.insertTransaksi(nama, jenis, jumlah, keterangan);
+
+        Log.d("form.transaksi", nama+"-"+Integer.toString(jenis)+"-"+Integer.toString(jumlah)
+                                +"-"+keterangan);
+        Toast.makeText(this, "Transaksi "+nama+" Berhasil disimpan", Toast.LENGTH_SHORT).show();
+
+        startActivity(new Intent(this, MainActivity.class));
+    }
+}
---
+++ AplikasiKeuangan/app/src/main/java/hafizh/him/aplikasikeuangan/Transaksi.java
@@ -0,0 +1,53 @@
+package hafizh.him.aplikasikeuangan;
+
+import android.provider.BaseColumns;
+
+/**
+ * Created by D2J-00 on 13/02/2017.
+ */
+public class Transaksi implements BaseColumns {
+
+    public String nama; // nama transaksi
+    public int jenis; // 1 = masukan, 2 = pengeluaran
+    public int jumlah;
+    public String keterangan;
+
+    public Transaksi(String nama, int jenis, int jumlah) {
+        this.nama = nama;
+        this.jenis = jenis;
+        this.jumlah = jumlah;
+    }
+
+    public Transaksi(String nama, int jenis, int jumlah, String keterangan) {
+        this.nama = nama;
+        this.jenis = jenis;
+        this.jumlah = jumlah;
+        this.keterangan = keterangan;
+    }
+
+    public String getJenis(){
+        if (this.jenis == 1)
+            return "pemasukan";
+        return "pengeluaran";
+    }
+
+    @Override
+    public String toString(){
+        return this.nama+": "+this.jumlah;
+    }
+
+    public static final String TABLE_NAME = "transaksi";
+    public static final String COL_NAMA = "nama";
+    public static final String COL_JENIS = "jenis";
+    public static final String COL_JUMLAH = "jumlah";
+    public static final String COL_KETERANGAN = "keterangan";
+
+    /*QUERY PEMBUATAN DAN PENGHAPUSAN TABLE */
+    public static final String SQL_CREATE = "create table "+TABLE_NAME
+                    +" ("+_ID+" integer primary_key,"
+                    +" "+COL_NAMA+" text,"
+                    +" "+COL_JENIS+" integer,"
+                    +" "+COL_JUMLAH+" integer,"
+                    +" "+COL_KETERANGAN+" text)";
+    public static final String SQL_DELETE = "drop table if exist"+TABLE_NAME;
+}
